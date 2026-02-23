65dbf712b8d5c64db9812b1a4be37ca57991407e
Jay Kumar <jaykumarr9022@gmail.com>
2026-02-23 10:44:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,15 @@
-
+const courses = {
+    foundation: [
+        {id: "maths1", name: "Mathematics for Data Science I"},
+        {id: "python", name: "Programming in Python"},
+        {id: "stats1", name: "Statistics for Data Science I"}
+    ],
+    diploma: [
+        {id: "dbms", name: "Database Management Systems"},
+        {id: "dsa", name: "Data Structures & Algorithms"}
+    ],
+    degree: [
+        {id: "ml", name: "Machine Learning"},
+        {id: "ai", name: "Artificial Intelligence"}
+    ]
+};
